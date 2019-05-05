3c59c7662b2780a8c3d454113eb1932f0e41e103
Marcin Stelmaszyk <stelmaszyk.m@gmail.com>
2019-05-05 12:08:44 +0200

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,11 +2,15 @@
 # Install HSTR
 
 
-echo "Copy DIRB repository"
+echo "## Copy DIRB repository"
 git clone https://github.com/icyfork/dirb.git
 
-echo "Copy DIRB executable to ~/.bashDirB"
+echo "## Copy DIRB executable to ~/.bashDirB"
 cp dirb/dirb.sh ~/.bashDirB
 
-echo "Cleaning directory"
+echo "## Append commands to ~/.bashrc"
+cat .bashrc >> ~/.bashrc
+
+echo "## Cleaning directory"
 rm -rf dirb/
+
