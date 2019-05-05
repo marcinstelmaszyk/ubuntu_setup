05637c9c06691ab44d0c36ad58ebb5c4463af5bf
Marcin Stelmaszyk <stelmaszyk.m@gmail.com>
2019-05-05 12:00:40 +0200

    Update configure.sh
    Abandoned creating tmp/. Cloning DIRB to the same directory as the script

--- configure.sh
+++ configure.sh
@@ -1,8 +1,12 @@
 
-# Create directory for temporary files
-mkdir ~/tmp
-
 # Install HSTR
 
-# Copy DIRB repository
-git clone https://github.com/icyfork/dirb.git ~/tmp/dirb/
+
+echo "Copy DIRB repository"
+git clone https://github.com/icyfork/dirb.git
+
+echo "Copy DIRB executable to ~/.bashDirB"
+cp dirb/dirb.sh ~/.bashDirB
+
+echo "Cleaning directory"
+rm -rf dirb/
